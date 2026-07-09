3667d12e93f0cf4f4e56fac0bdeca620873cc2a7
siva-prashanthi <sivaprashanthi1234@gmail.com>
2026-07-09 12:17:29 +0530

    Added Two Sum IV solution

---
+++ two_sum_iv_bst.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTarget(self, root:optional[Treenode],k:int)->bool:
+        seen=set()
+        def dfs(node):
+            if not node:
+                return False
+            if k-node.val in seen:
+                return True
+            seen.add(node.val)
+            return dfs(node.left) or dfs(node.right)
+        return dfs(root)

dfa5b2f09190b1e5fb5f74244288b38cb81833d3
siva-prashanthi <sivaprashanthi1234@gmail.com>
2026-07-16 20:51:06 +0530

    Added countsquaresumtriples solution

---
+++ countsquaresumtriples.py
@@ -0,0 +1,10 @@
+from math import sqrt
+class Solution:
+    def countTriples(self,n:List[int])->int:
+        res=0
+        for a in range(1,n+1):
+            for b in range(1,n+1):
+                c=int(sqrt(a**2+b**2+1))
+                if c<=n and c**2==a**2+b**2:
+                    res+=1
+        return res
